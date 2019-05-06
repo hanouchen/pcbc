960bec6256234b74569e72e0b243960b4f373b55
hanouchen <chenhanou13@gmail.com>
2019-05-06 22:06:01 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let options = [];
+
+$( '.dropdown-menu a' ).on( 'click', function( event ) {
+
+   var $target = $( event.currentTarget );
+   const value = $target.attr( 'data-value' );
+   var $inp = $target.find( 'input' );
+   let idx;
+
+   if ( ( idx = options.indexOf( val ) ) > -1 ) {
+      options.splice( idx, 1 );
+    //   setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+       $inp.prop( 'checked', false );
+   } else {
+      options.push( val );
+    //   setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+       $inp.prop( 'checked', true );
+   }
+
+   $( event.target ).blur();
+      
+   console.log( options );
+   return false;
+});
